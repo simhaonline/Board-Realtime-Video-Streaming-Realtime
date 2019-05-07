29b8ec5f0dca917e68d94d601ae4b2ab3de4b2cf
JeiDevp <joseanyeloserrano@gmail.com>
2019-05-07 14:28:23 -0400

    Improvement bug

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,13 @@
 var socket = io();
-
+var color = "blue"
 function drawPen(ctx, x, y, click) {
     // ctx.clearRect(0, 0, ctx.canvas.width, ctx.height)
-    ctx.strokeStyle = "#df4b26";
+    ctx.strokeStyle = color;
     ctx.lineJoin = "round";
     ctx.lineWidth = 5;
     for (let i = 0; i < x.length; i++) {
         ctx.beginPath();
-
+        ctx.lineCap = "round"; 
         if (click[i] && i) ctx.moveTo(x[i - 1], y[i - 1]);
         else ctx.moveTo(x[i] - 1, y[i]);
 
